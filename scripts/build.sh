7791e394972a6951f90ec7edcbbfb635d11dcbcb
Danil Troshnev <denergytro@gmail.com>
2019-02-03 01:31:07 +0700

    setup updated (tested on test.pypi.org)

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,11 @@
 
 PROJECT_DIR=$( dirname $( dirname $( realpath $0 ) ) )
 BUILD_DIR=$PROJECT_DIR/build
+DIST_DIR=$PROJECT_DIR/dist
 
-rm -rf $BUILD_DIR && python $PROJECT_DIR/setup.py build
+# rm -rf $BUILD_DIR && python $PROJECT_DIR/setup.py build
+rm -rf $BUILD_DIR $DIST_DIR
+
+python $PROJECT_DIR/setup.py sdist bdist_wheel
+
+rm -rf $BUILD_DIR $PROJECT_DIR/src/*.egg-info
--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3.7
 
+from pathlib import Path
 from setuptools import setup, find_packages
+
+SETUP_FILE = Path(__file__)
+PROJECT_DIR = SETUP_FILE.parent
 
 LIB_NAME = 'configoo'
 
 
+def load_text(path: Path) -> str:
+    with path.open('r') as fd:
+        return fd.read()
+
+
 setup(
     name=LIB_NAME,
-    version='0.0.1',
+    version='0.0.3',
     description='Python config lib for loading application configurations',
+    long_description=load_text(PROJECT_DIR / 'README.md'),
+    long_description_content_type='text/markdown',
     author='Danil Troshnev',
     author_email='denergytro@gmail.com',
     url=f'https://bitbucket.org/Zerlok/{LIB_NAME}/',
@@ -21,9 +32,8 @@ setup(
     extras_require={
         'env': [],
         'dotenv': [
-            'python-dotenv~=0.10',
+            'python-dotenv >= 0.10.1',
         ],
         'json': [],
-        # 'yaml': ['yaml'],
     },
 )
